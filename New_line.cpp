8283a8a9c2054a7e333a720db82286c80e844b26
Poonji <77487602+Poonji@users.noreply.github.com>
2023-07-10 15:26:53 +0530

    Create New_line.cpp

---
+++ New_line.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void printNewLine();
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+// Function to print each word in new line
+void printNewLine(){
+    
+    // Your code here
+    cout<<"Geeks"<<endl<<"for"<<endl<<"Geeks";
+    
+}
