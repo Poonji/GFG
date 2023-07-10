6d7851d786372288d5f1ff7fe0ac0623b6d71505
Poonji <77487602+Poonji@users.noreply.github.com>
2023-07-10 15:28:20 +0530

    Create C++ Array (print an element) | Set 2.cpp

---
+++ C++ Array (print an element) | Set 2.cpp
@@ -0,0 +1,21 @@
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int findElementAtIndex(int a[10], int n, int key) 
+    {
+       /* int i;
+        cin>>n>>key;
+        for(i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        if(key==i)
+        return a[i];*/
+        int num=a[key];
+        return num;
+    }
+};
