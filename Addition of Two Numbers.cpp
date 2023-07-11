104952ac8474411a697ed9275a19e28ab32895cf
Poonji <77487602+Poonji@users.noreply.github.com>
2023-07-11 12:05:30 +0530

    Create Addition of Two Numbers.cpp

---
+++ Addition of Two Numbers.cpp
@@ -0,0 +1,11 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int addition(int A, int B){
+        return A+B;
+    }
+};
