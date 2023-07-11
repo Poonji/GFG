5517192947ad7cb82b8948518c649c3a333707ca
Poonji <77487602+Poonji@users.noreply.github.com>
2023-07-11 12:03:54 +0530

    Create C++ Input  Output.cpp

---
+++ C++ Input  Output.cpp
@@ -0,0 +1,13 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int multiplication(int A, int B){
+        int C;
+        C=A*B;
+        return C;
+    }
+};
