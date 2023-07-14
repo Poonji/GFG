c55c17f313c8679b775129cb5ea3d47422c081ad
Poonji <77487602+Poonji@users.noreply.github.com>
2023-07-14 11:42:59 +0530

    Create C++ 2-D Arrays | Set-1.cpp

---
+++ C++ 2-D Arrays | Set-1.cpp
@@ -0,0 +1,16 @@
+
+
+vector<vector<int>> transpose(int a[][M], int n)
+{
+    vector<vector<int>>ans;
+    for(int i=0;i<n;i++)
+    {
+        vector<int>temp;
+        for(int j=0;j<n;j++)
+        {
+            temp.push_back(a[j][i]);
+        }
+        ans.push_back(temp);
+    }
+    return ans;
+}
