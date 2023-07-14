07c999f00666d03b7199164cb71380b3a35d96c9
Poonji <77487602+Poonji@users.noreply.github.com>
2023-07-14 11:30:12 +0530

    Create Replace all 0's with 5.cpp

---
+++ Replace all 0's with 5.cpp
@@ -0,0 +1,15 @@
+class Solution{
+  public:
+    int convertFive(int n)
+    {
+        string str=to_string(n);
+        for(int i=0;i<str.length();i++){
+            if(str[i]=='0')
+            {
+                str[i]='5';
+            }
+        }
+        int ans=stoi(str);
+        return ans;
+    }
+};
