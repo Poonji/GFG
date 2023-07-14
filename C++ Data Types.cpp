4e73015ef7f3f5226175042581f70336d7c83538
Poonji <77487602+Poonji@users.noreply.github.com>
2023-07-14 11:32:59 +0530

    Create C++ Data Types.cpp

---
+++ C++ Data Types.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int cppIntType() {
+        int a;
+        cin>>a;
+        return a;
+        cout<<endl;
+    }
+    
+    char cppCharType() {
+        char ch;
+        cin>>ch;
+        return ch;
+        cout<<endl;
+        
+    }
+    
+    float cppFloatType() {
+        float f;
+        cin>>f;
+        return f;
+        
+    }
+};
