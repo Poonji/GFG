99a28f0d7ee7f6a0604ec73972643eaf33806b82
Poonji <77487602+Poonji@users.noreply.github.com>
2023-07-21 22:51:21 +0530

    Create Move all negative elements to end.cpp

---
+++ Move all negative elements to end.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    void segregateElements(int arr[],int n)
+    {
+        vector<int>v;
+        for(int i=0;i<n;i++)
+        {
+         if(arr[i]>0)
+         {
+            v.push_back(arr[i]);
+         }
+        }
+        for(int i=0;i<n;i++)
+        {
+         if(arr[i]<0)
+         {
+            v.push_back(arr[i]);
+         }
+        }
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=v[i];
+        }
+        
+    }
+};
