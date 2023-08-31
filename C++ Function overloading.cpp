6d34e47769639e841be41b5a1d2a1151356aa4b7
Poonji <poonjij21@gmail.com>
2023-08-31 08:21:39 +0530

    Create C++ Function overloading.cpp

---
+++ C++ Function overloading.cpp
@@ -0,0 +1,16 @@
+
+void volume(int s)
+{
+    cout<<s*s*s<<endl;
+}
+
+void volume(int r, int h)
+{
+    cout<<3.14159*r*r*h<<endl;
+ 
+}
+
+void volume(int l, int b, int h)
+{
+    cout<<l*b*h<<endl;
+}
