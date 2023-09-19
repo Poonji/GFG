11f91c970f6bd85fac51782b3262f0a68da4e1ef
Poonji <poonjij21@gmail.com>
2023-09-19 22:57:29 +0530

    Create Remove duplicate element from sorted Linked List.cpp

---
+++ Remove duplicate element from sorted Linked List.cpp
@@ -0,0 +1,20 @@
+Node *removeDuplicates(Node *head)
+{
+    if(head==NULL)
+    {
+        return NULL;
+    }
+    Node*curr=head;
+    while(curr!=NULL && curr->next!=NULL)
+    {
+        if(curr->data==curr->next->data)
+        {
+            delete(curr->next);
+            curr->next=curr->next->next;
+        }
+        else{
+            curr=curr->next;
+        }
+    }
+    return head;
+}
