cbfd8870c0b73b788d94ee0c5b0125a10f57a20d
Poonji <poonjij21@gmail.com>
2023-10-04 23:00:41 +0530

    Create Two Dimensional World.cpp

---
+++ Two Dimensional World.cpp
@@ -0,0 +1,11 @@
+void twoDimensional(int*ptr[],int N){
+    for(int i = 0;i<N;i++){
+        for(int j = 0;j<N;j++){
+            
+        cout<<(ptr[i][j])<<" ";
+            
+        }
+        cout << endl;
+    }
+    
+}
