57b8952daaa37e778c00245072e2b90193eae770
Poonji <poonjij21@gmail.com>
2023-10-05 23:43:43 +0530

    Create Selection Sort.cpp

---
+++ Selection Sort.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+    public:
+    int select(int arr[], int i)
+    {
+        // code here such that selectionSort() sorts arr[]
+    }
+     
+    void selectionSort(int arr[], int n)
+    {
+         for(int i=0;i<n-1;i++)
+        {
+            int minIndex=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[minIndex]){
+                    minIndex=j;
+                }
+            }
+            swap(arr[i],arr[minIndex]);
+        }
+    }
+};
