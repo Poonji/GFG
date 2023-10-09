86cfd5e75c7c054043b45d9412d1ea289509de3d
Poonji <poonjij21@gmail.com>
2023-10-09 22:31:51 +0530

    Create Peak element.cpp

---
+++ Peak element.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+    public:
+    int peakElement(int arr[], int n)
+    {
+        int start=0;
+
+      int end=n-1;
+
+      while(start<end)
+
+      {
+
+          int mid=(start+end)/2;
+
+          if(arr[mid]>arr[mid+1])
+
+          {
+
+              end=mid;
+
+          }
+
+          else
+
+          {
+
+              start=mid+=1;
+
+          }
+
+      }
+
+      return start;
+
+    }
+};
