1a51153e57eb713db6d2f8f33a37c06d8e19072d
Poonji Jadon <poonjij21@gmail.com>
2024-03-23 09:20:33 +0530

    Create Fibonacci series up to Nth term.cpp

---
+++ Fibonacci series up to Nth term.cpp
@@ -0,0 +1,18 @@
+  public:
+    vector<int> Series(int n) {
+         vector<int> ans;
+        int a = 0, b = 1;
+        ans.push_back(a);
+        ans.push_back(b);
+        if (n == 1){
+            return ans;
+        }
+        for (int i=0; i<n-1; i++){
+            int temp = (a+b)%1000000007;
+            ans.push_back(temp);
+            a = b;
+            b = temp;
+        }
+        return ans;
+    }
+};
